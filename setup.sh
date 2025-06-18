26f9bf6c1d87526d26e2cd17b3ca5c2490ad3898
THE-MOLECULAR-MAN <>
2025-06-18 09:36:13 -0400

    initial version

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+# Tim H 2025
+
+set -e # bail on error
+
+# MacOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install --formula --quiet git gh elfio findutils
+    gfind . -type f -iname '*.sh' ! -executable -exec chmod +x "{}" \;
+
+
+# Ubuntu
+elif [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    if [[ "$ID" == "ubuntu" ]]; then
+        sudo apt-get update
+        sudo apt install -y build-essential cmake wget git gh distcc elfio
+    else
+        echo "Other Linux: $ID"
+        exit 1
+    fi
+    find . -type f -iname '*.sh' ! -executable -exec chmod +x "{}" \;
+else
+    echo "Unknown OS"
+    exit 2
+fi
