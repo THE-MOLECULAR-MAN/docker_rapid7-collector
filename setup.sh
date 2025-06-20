c6e50c55ecd2a158f49bde730e7d44880390ef9d
THE-MOLECULAR-MAN <>
2025-06-20 09:08:21 -0400

    fixed some missing dependencies

--- setup.sh
+++ setup.sh
@@ -14,7 +14,9 @@ elif [[ -f /etc/os-release ]]; then
     . /etc/os-release
     if [[ "$ID" == "ubuntu" ]]; then
         sudo apt-get update
-        sudo apt install -y build-essential cmake wget git gh distcc elfio
+        # sudo apt install -y build-essential cmake wget git gh distcc binutils elfutils libdw-dev patchelf expect autoexpect
+        
+
     else
         echo "Other Linux: $ID"
         exit 1
